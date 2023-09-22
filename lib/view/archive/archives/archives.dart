f8b22503026b4ff973be60f451e381b2d6d98a04
Mahmoud ElSobky <elsobakymahmoud@gmail.com>
2023-09-22 22:05:13 +0300

    22/9

--- lib/view/archive/archives/archives.dart
+++ lib/view/archive/archives/archives.dart
@@ -35,7 +35,7 @@ class Archives extends StatelessWidget {
                     children: [
                       Expanded(
                         child: Center(
-                          child: Text(style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 23), 'المواعيد'),
+                          child: Text(style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 23), 'التقرير الشهري'),
                         ),
                       ),
                     ],
@@ -43,60 +43,68 @@ class Archives extends StatelessWidget {
                 ),
               ),
               const SizedBox(height: 40),
-              Container(
-                height: 70,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(50),
-                  color: Colors.blue,
-                ),
-                child: MaterialButton(
-                  onPressed: () {
-                    Navigator.push(context, MaterialPageRoute(builder: (builder) => const ArchivesFixtures()));
-                  },
-                  child: const Row(
-                    children: [
-                      Expanded(
-                        child: Center(
-                          child: Text(
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 22,
+              Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      height: 70,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        color: Colors.blue,
+                      ),
+                      child: MaterialButton(
+                        onPressed: () {
+                          Navigator.push(context, MaterialPageRoute(builder: (builder) => const ArchivesFixtures()));
+                        },
+                        child: const Row(
+                          children: [
+                            Expanded(
+                              child: Center(
+                                child: Text(
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 22,
+                                    ),
+                                    'التركيبات'),
                               ),
-                              'التركيبات'),
+                            ),
+                          ],
                         ),
                       ),
-                    ],
+                    ),
                   ),
-                ),
-              ),
-              const SizedBox(height: 40),
-              Container(
-                height: 70,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(50),
-                  color: Colors.blue,
-                ),
-                child: MaterialButton(
-                  onPressed: () {
-                    // Navigator.push(context, MaterialPageRoute(builder: (builder) => const ArchiveExpenses()));
-                  },
-                  child: const Row(
-                    children: [
-                      Expanded(
-                        child: Center(
-                          child: Text(
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 22,
+                  const SizedBox(width: 15),
+                  Expanded(
+                    child: Container(
+                      height: 70,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        color: Colors.blue,
+                      ),
+                      child: MaterialButton(
+                        onPressed: () {
+                          // Navigator.push(context, MaterialPageRoute(builder: (builder) => const ArchiveExpenses()));
+                        },
+                        child: const Row(
+                          children: [
+                            Expanded(
+                              child: Center(
+                                child: Text(
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 22,
+                                    ),
+                                    'المصروفات'),
                               ),
-                              'المصروفات'),
+                            ),
+                          ],
                         ),
                       ),
-                    ],
+                    ),
                   ),
-                ),
+                ],
               ),
             ],
           ),
